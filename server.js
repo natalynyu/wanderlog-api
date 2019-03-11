64757324fac4646df387ee018efe663b27cb23d8
natalynyu <natalyn.yu@gmail.com>
2019-03-11 10:34:52 -0400

    include itinerary routes in server.js
    register itinerary routes under server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const itineraryRoutes = require('./app/routes/itinerary_routes')
 
 // require error handling middleware
 const errorHandler = require('./lib/error_handler')
@@ -74,6 +75,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(itineraryRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
